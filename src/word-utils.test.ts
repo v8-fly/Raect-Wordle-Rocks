878f491d5d295ee2bc678c75316bfccc480e0e26
v8-fly <98163507+v8-fly@users.noreply.github.com>
2022-07-23 23:25:33 +0530

    computeGuess initial implementation

--- src/word-utils.test.ts
+++ src/word-utils.test.ts
@@ -1,10 +1,39 @@
 import { describe, expect, it } from "vitest"
-import { getRandomWord } from "./word-utils"
-import { render, screen, userEvent } from "./test/test-utils"
+import { getRandomWord, computeGuess, LettersState } from "./word-utils"
 
-describe("word-utils", () => {
+describe("getRandomWord", () => {
   it("get random word", () => {
     expect(getRandomWord()).toBeTruthy()
     expect(getRandomWord().length).toBe(5)
+  })
+})
+
+describe("computeGuess", () => {
+  it("works with match and present", () => {
+    expect(computeGuess("boost", "basic")).toEqual([
+      LettersState.Match,
+      LettersState.Miss,
+      LettersState.Miss,
+      LettersState.Pesent,
+      LettersState.Miss,
+    ])
+  })
+  it("works with all matches", () => {
+    expect(computeGuess("boost", "boost")).toEqual([
+      LettersState.Match,
+      LettersState.Match,
+      LettersState.Match,
+      LettersState.Match,
+      LettersState.Match,
+    ])
+  })
+  it("works with full Miss", () => {
+    expect(computeGuess("azzzz", "boost")).toEqual([
+      LettersState.Miss,
+      LettersState.Miss,
+      LettersState.Miss,
+      LettersState.Miss,
+      LettersState.Miss,
+    ])
   })
 })
--- src/word-utils.ts
+++ src/word-utils.ts
@@ -1,8 +1,30 @@
 import wordBank from "./wordbank.json"
 
+enum LettersState {
+  Miss = "Miss",
+  Pesent = "Pesent",
+  Match = "Match",
+}
+
 const getRandomWord = () => {
   const randomIndex = Math.floor(Math.random() * wordBank.length)
   return wordBank[randomIndex]
 }
 
-export { getRandomWord }
+const computeGuess = (guess: string, answerString: string): LettersState[] => {
+  const result: LettersState[] = []
+
+  const guessArray = guess.split("")
+  const answerArray = answerString.split("")
+  guessArray.forEach((letter, index) => {
+    if (letter === answerArray[index]) {
+      result.push(LettersState.Match)
+    } else if (answerArray.includes(letter)) {
+      result.push(LettersState.Pesent)
+    } else result.push(LettersState.Miss)
+  })
+
+  return result
+}
+
+export { getRandomWord, computeGuess, LettersState }
